f4daafce73861a6456c685b5d706f393a8f81592
Arda Eren <Poedx2@gmail.com>
2023-04-16 11:11:39 +0300

    dark mod and stuff

---
+++ Assets/JQuery/Dark-Mode.js
@@ -0,0 +1,4 @@
+$(document).ready(function () {
+    $("*").addClass("text-bg-dark");
+    $(".carousel,.carousel-indicators,.carousel-inner,.carousel-item,.carousel-control-prev,.carousel-control-next,.carousel-control-next-icon,.carousel-control-prev-icon,.carousel-caption").removeClass("text-bg-dark");
+});
