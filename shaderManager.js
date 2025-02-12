37da13b872a80008a8726321440bc8f63630d9f1
Arda Eren <Poedx2@gmail.com>
2025-02-12 22:28:29 +0300

    Changes to be committed: 	new file:   Minecraft/Images/SkeletonDrinking.png 	new file:   Minecraft/maparts.html 	modified:   main.css 	modified:   shaderManager.js

--- shaderManager.js
+++ shaderManager.js
@@ -1,1 +1,9 @@
-document.getElementsByTagName("canvas");
+let canvases = document.getElementsByTagName("canvas");
+for (let canvas of canvases) {
+    let shaderPath = canvas.getAttribute("id");
+    const gl = canvas.getContext("webgl");
+    // Set clear color to black, fully opaque
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    // Clear the color buffer with specified clear color
+    gl.clear(gl.COLOR_BUFFER_BIT);
+};
